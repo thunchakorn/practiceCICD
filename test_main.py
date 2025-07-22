9693f7bd2b07c978a95ae768f2cb95bc5c031741
thunchakorn-findx <thunchakorn@findx.co.th>
2025-07-22 16:16:43 +0700

    add testing to cicd

--- test_main.py
+++ test_main.py
@@ -1,5 +1,6 @@
 from fastapi.testclient import TestClient
 from main import app
+import psycopg2
 
 client = TestClient(app)
 
@@ -14,3 +15,14 @@ def test_read_item():
     response = client.get("/items/42?q=example")
     assert response.status_code == 200
     assert response.json() == {"item_id": 42, "q": "example"}
+
+
+def test_db_connection():
+    conn = psycopg2.connect(
+        host="localhost",
+        port=5436,
+        user="username",
+        password="pass123",
+        dbname="test_db",
+    )
+    assert conn.status == psycopg2.extensions.STATUS_READY
